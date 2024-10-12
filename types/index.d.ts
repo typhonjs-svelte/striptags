1f8fe2978a6cb1a56d519aafa1ca9112277bf438
typhonrt <support@typhonjs.io>
2024-10-11 20:55:56 -0700

    Update types.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,16 @@
 interface StateMachineOptions {
-    readonly allowedTags?: Set<string>;
-    readonly disallowedTags?: Set<string>;
-    readonly tagReplacementText: string;
-    readonly encodePlaintextTagDelimiters: boolean;
+  readonly allowedTags?: Set<string>;
+  readonly disallowedTags?: Set<string>;
+  readonly tagReplacementText: string;
+  readonly encodePlaintextTagDelimiters: boolean;
 }
 
 declare const DefaultStateMachineOptions: StateMachineOptions;
 declare class StateMachine {
-    private state;
-    private transitionFunction;
-    constructor(partialOptions?: Partial<StateMachineOptions>);
-    consume(text: string): string;
+  private state;
+  private transitionFunction;
+  constructor(partialOptions?: Partial<StateMachineOptions>);
+  consume(text: string): string;
 }
 declare function striptags(text: string, options?: Partial<StateMachineOptions>): string;
 
